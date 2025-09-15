e82176a713d377f1f35ed2eb395bd03f4d80bc31
hirunadissanyake <hirunasankalpa@gmail.com>
2025-09-15 10:36:22 +0530

    first-react-app

---
+++ React/my-first-app/src/App.tsx
@@ -0,0 +1,29 @@
+
+// functional componenet
+
+import './App.css'
+
+function App() {
+  //jsx: javascript xml (js,ts + html,xml)
+  //tsx
+  return (
+    <>
+      <h1>Hello React</h1>
+    </>
+  )
+}
+
+export default App
+
+
+// class componenet
+// js walin inhertance use karala 
+
+// import { Component } from "react";
+// class App extends Component{
+//   render(){
+//       return <h1> Hello React </h1>
+//   }
+// }
+
+// export default App
